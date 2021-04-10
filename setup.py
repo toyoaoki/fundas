522e19f885f5a1e7b43d0b00adee2ad974fb7f61
toyoaoki <toyohiro.aoki.engineering@gmail.com>
2021-04-10 19:05:21 +0900

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="fundas",
+    version='0.0',
+    description='A data analysis library that applies functorial data model',
+    author='Toyohiro Aoki',
+    author_email='toyohiro.aoki.engineering@gmail.com',
+    url='https://github.com/toyoaoki/fundas',
+)
